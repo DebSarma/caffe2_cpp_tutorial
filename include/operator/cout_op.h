498b10ca84c333d7ce676461d0d0d91f86407efd
Leo <leonardvandriel@gmail.com>
2017-07-08 13:08:11 -0700

    add cout cudnn

--- include/operator/cout_op.h
+++ include/operator/cout_op.h
@@ -50,4 +50,48 @@ SHOULD_NOT_DO_GRADIENT(Cout);
 
 }  // namespace caffe2
 
+#ifdef WITH_CUDA
+
+#include "caffe2/core/common_cudnn.h"
+#include "caffe2/core/context_gpu.h"
+#include "caffe2/core/types.h"
+
+namespace caffe2 {
+
+template <typename T>
+class CuDNNCoutOp final : public Operator<CUDAContext> {
+ public:
+  explicit CuDNNCoutOp(const OperatorDef& def, Workspace* ws)
+      : Operator<CUDAContext>(def, ws),
+      titles(def.input_size()) {
+    for (auto i = 0; i < titles.size(); i++) {
+        titles[i] = def.input(i);
+    }
+  }
+
+  bool RunOnDevice() override {
+    auto index = 0;
+    for (auto &title: titles) {
+      auto tensor = Input(index++);
+      std::cout << title << " (" << tensor.dims() << "): ";
+      print(tensor);
+      std::cout << std::endl;
+    }
+    return true;
+  }
+
+ protected:
+  std::vector<string> titles;
+};
+
+namespace {
+
+REGISTER_CUDNN_OPERATOR(Cout, CuDNNCoutOp<float>);
+
+}  // namespace
+
+}  // namespace caffe2
+
+#endif
+
 #endif  // COUT_OP_H
